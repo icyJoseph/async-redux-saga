88e231bc9b60cddbfa5ab6c99741445d57857872
Joseph Chamochumbi <sephxd1234@gmail.com>
2019-04-03 15:57:56 +0200

    test: basic Dashboard test using rtl

--- src/components/Dashboard.spec.js
+++ src/components/Dashboard.spec.js
@@ -1,1 +1,11 @@
 // Must have at least one test file in this directory or Mocha will throw an error.
+import React from "react";
+import { render } from "react-testing-library";
+import Dashboard from "./Dashboard";
+
+describe("Dashboard", () => {
+  const { container } = render(<Dashboard />);
+  it("renders", () => {
+    expect(container).toBeDefined();
+  });
+});
